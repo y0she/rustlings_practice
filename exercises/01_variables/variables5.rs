b9d538a4fcecd1e64a7b8756a7b9d7463e5e5467
y0she <y.kaneshiro0303@gmail.com>
2024-09-04 18:00:41 +0000

    done 01_variables

--- exercises/01_variables/variables5.rs
+++ exercises/01_variables/variables5.rs
@@ -2,7 +2,9 @@ fn main() {
     let number = "T-H-R-E-E"; // Don't change this line
     println!("Spell a number: {}", number);
 
-    // TODO: Fix the compiler error by changing the line below without renaming the variable.
-    number = 3;
-    println!("Number plus two is: {}", number + 2);
+    // TODO: Fix the compiler error by changing the line below without renaming 『the variable.
+    {
+        let number = 3;
+        println!("Number plus two is: {}", number + 2);
+    }
 }
